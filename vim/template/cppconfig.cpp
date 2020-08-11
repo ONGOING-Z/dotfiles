321ab41ffbb0aa3b220f267c84f2e373aee0aba4
ongoing-z <code@ongoingblog.org>
2020-08-11 12:05:58 +0800

    udpate the template

--- vim/template/cppconfig.cpp
+++ vim/template/cppconfig.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 /* Leetcode */
@@ -15,4 +16,9 @@ using namespace std;
 
 
 /* 一些总结 */
-
+// 1. 题意: 
+//
+// 需要注意的点: 
+// 1. 
+// 2. 
+// 3. 
